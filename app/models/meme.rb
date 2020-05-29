2becc656082e5809823d57773d01ac5a5cd48290
sebas_z <zanabria.s@pucp.pe>
2020-05-29 18:16:24 -0500

    Validations completed

--- app/models/meme.rb
+++ app/models/meme.rb
@@ -5,7 +5,10 @@ class Meme < ApplicationRecord
   has_many :commentators, through: :comments, source: :user
   has_many :votes
   has_many :voters, through: :votes, source: :user
-  
+  validates :title, presence: true
+  validates :title, uniqueness: true
+  validates :url_source, presence: true, format: { with: /\A.+\.(?:png|jpg|gif)\z/i, message: "%{value} should be .png, .jpg or .gif"}
   validates :votes_count, numericality: {only_integer: true, greater_than_or_equal_to: 0}
   validates :comments_count, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  validates :meme_type, inclusion: { in: %w(image gif), message: "%{value} is not a valid meme type" }
 end
