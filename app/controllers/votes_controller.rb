4f2de87f03b6220b25d0c8fbc4471344a720de5f
unknown <wilber.carrascal@gmail.com>
2020-05-30 15:30:03 -0500

    set_votes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,9 +1,13 @@
 class VotesController < ApplicationController
   def create
-    puts '<----------------------------------------------------------------------->'
-    puts params[:meme_id]
-    puts current_user.id
-    puts '<----------------------------------------------------------------------->'
-    #Votes.create(meme_id: params[:meme_id],)
+    meme_id = params[:meme_id].to_i
+    user_id = current_user.id
+    vote = Vote.find_by(meme_id: meme_id, user_id: user_id)
+    if vote
+      vote.destroy
+    else
+      Vote.create(meme_id: meme_id, user_id: user_id)
+    end
+    redirect_to meme_url(meme_id)
   end
 end
